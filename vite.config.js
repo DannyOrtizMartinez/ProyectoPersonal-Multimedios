7e393a7bbb47e784ff0626b0f2fd4cba66d99f21
DannyOrtizMartinez <DANNY.ORTIZMARTINEZ@ucr.ac.cr>
2026-06-18 10:01:17 -0600

    Solucion a github pg

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  // Esta barra es crucial al inicio y al final
   base: '/ProyectoPersonal-Multimedios/',
 })
